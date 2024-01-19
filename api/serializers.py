4bddf81b6a6fc1cbe8f8dfadd8b356000beb90f1
Вячеслав Алисов <v.alisov@alta-west.ru>
2024-01-19 10:16:31 +0300

    ыуеештпы ыук гзв

--- api/serializers.py
+++ api/serializers.py
@@ -139,8 +139,28 @@ class SettingSerializer(serializers.ModelSerializer):
             "id",
             "key",
             "type",
-            "value",
+            "value_string",
+            "value_boolean",
+            "value_number",
+            "predefined_key",
+            "custom_key",
         ]
+
+    def validate(self, data):
+        if not data.get("predefined_key") and not data.get("custom_key"):
+            raise serializers.ValidationError(
+                "Необходимо указать либо предопределенный ключ, либо пользовательский ключ."
+            )
+        if data.get("predefined_key") and data.get("custom_key"):
+            raise serializers.ValidationError(
+                "Укажите только один ключ: либо предопределенный, либо пользовательский."
+            )
+        return data
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["value"] = instance.get_value()
+        return representation
 
 
 class CitySerializer(serializers.ModelSerializer):
