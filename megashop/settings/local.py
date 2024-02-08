7026a4223fe6e39515d9d35e541a76a91a39d93c
Вячеслав Алисов <v.alisov@alta-west.ru>
2024-02-08 10:34:53 +0300

    added allowed host

--- megashop/settings/local.py
+++ megashop/settings/local.py
@@ -2,6 +2,6 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["phone.altawest.ru", "94.241.142.109", "0.0.0.0", "127.0.0.1", "dev-api-shop.altawest.ru"]
+ALLOWED_HOSTS = ["phone.altawest.ru", "94.241.142.109", "0.0.0.0", "127.0.0.1", "dev-api-shop.altawest.ru", 'web']
 
 CELERY_BROKER_URL = "redis://cache:6379/0"
