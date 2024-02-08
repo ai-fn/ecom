33dad389b970633d3251e11d6d929293180e6e02
Вячеслав Алисов <v.alisov@alta-west.ru>
2024-02-08 10:28:13 +0300

    removed error config

--- megashop/settings/base.py
+++ megashop/settings/base.py
@@ -65,26 +65,6 @@ INSTALLED_APPS = [
     "api",
 ]
 
-PROMETHEUS_LATENCY_BUCKETS = (
-    0.01,
-    0.025,
-    0.05,
-    0.075,
-    0.1,
-    0.25,
-    0.5,
-    0.75,
-    1.0,
-    2.5,
-    5.0,
-    7.5,
-    10.0,
-    25.0,
-    50.0,
-    75.0,
-    float("inf"),
-)
-PROMETHEUS_METRIC_NAMESPACE = "megashop_backend"
 AUTH_USER_MODEL = "account.CustomUser"
 
 MIDDLEWARE = [
