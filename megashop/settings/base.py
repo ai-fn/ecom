389c772bf0157faefd097624db8568053cf474f5
Вячеслав Алисов <v.alisov@alta-west.ru>
2024-02-20 10:44:38 +0300

    page size changed

--- megashop/settings/base.py
+++ megashop/settings/base.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": 32,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
