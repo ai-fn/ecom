c61b6005de806728e17f59178d6d8c0ddf1b3077
Вячеслав Алисов <v.alisov@alta-west.ru>
2024-02-27 17:28:37 +0300

    added photos in search results

--- api/serializers/_document.py
+++ api/serializers/_document.py
@@ -1,18 +1,35 @@
 from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
+from api.serializers.product_image import ProductImageSerializer
 
 from shop.documents import CategoryDocument, ProductDocument, ReviewDocument
+from rest_framework import serializers
+
+from shop.models import Product
+
+
+class ImageSerializer(serializers.Serializer):
+    image = serializers.ImageField()
 
 
 class CategoryDocumentSerializer(DocumentSerializer):
+    image = serializers.ImageField()
+
     class Meta:
         document = CategoryDocument
         fields = "__all__"
 
 
-class ProductDocumentSerializer(DocumentSerializer):
+class ProductDocumentSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
+
     class Meta:
-        document = ProductDocument
-        fields = "__all__"
+        model = Product
+        fields = [
+            "id",
+            "title",
+            "description",
+            "images",  # Ensure images are included
+        ]
 
 
 class ReviewDocumentSerializer(DocumentSerializer):
