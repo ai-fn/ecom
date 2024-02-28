053add852bdc520e693fe9bcd96786b688616c00
Вячеслав Алисов <v.alisov@alta-west.ru>
2024-02-28 10:50:43 +0000

    Update base.py

--- megashop/settings/base.py
+++ megashop/settings/base.py
@@ -207,7 +207,7 @@ SPECTACULAR_SETTINGS = {
 }
 ELASTICSEARCH_DSL = {
     "default": {
-        "hosts": "http://localhost:9200",
+        "hosts": "http://elastic:9200",
         # "http_auth": ("elastic", "k1fjic392h9io"),
     }
 }
