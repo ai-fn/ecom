d905a8aefd699e30e417db8a366460fa0b343437
Андрей Жиров <a.zhirov@alta-west.ru>
2024-03-15 09:27:00 +0300

    13721: products by ids list

--- api/urls.py
+++ api/urls.py
@@ -11,6 +11,7 @@ from api.views import (
     DataExportView,
     MyTokenObtainPairView,
     OrderViewSet,
+    ProductsById,
     PriceViewSet,
     ProductViewSet,
     ProductsInOrderViewSet,
@@ -44,8 +45,8 @@ router.register(r"brands", BrandView)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("shop/", include('shop.urls')),
-    path("cart/", include('cart.urls')),
+    path("shop/", include("shop.urls")),
+    path("cart/", include("cart.urls")),
     path("token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     re_path(
@@ -56,4 +57,5 @@ urlpatterns = [
     re_path(r"^export", DataExportView.as_view()),
     path("register/", UserRegistrationView.as_view(), name="register"),
     path("search/", GeneralSearchView.as_view(), name="general-search"),
+    path("products_by_id_list/", ProductsById.as_view(), name="products_bu_id_list"),
 ]
