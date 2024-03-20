4e1c3b0477825f46674ffe1e3f682e3a992ffa7e
Андрей Жиров <a.zhirov@alta-west.ru>
2024-03-20 16:57:59 +0300

    13831: confirm code

--- api/views/confirm_register.py
+++ api/views/confirm_register.py
@@ -19,20 +19,22 @@ from ..serializers.confirm_code import ConfirmCodeSerializer
 from account.models import CustomUser
 from drf_spectacular.utils import extend_schema
 
+code_lifetime = int(settings.CONFIRM_CODE_LIFE_TIME)
+
 
 @extend_schema(
-    tags=['Account'],
-    description="Отпарвка СМС сообщения. Кэширование запроса на 60 секунд.",
+    tags=["Account"],
+    description=f"Отпарвка СМС сообщения. Кэширование запроса на {code_lifetime} секунд.",
     summary="Отпарвка СМС сообщения",
-    responses=[{"409": "CONFLICT"}, {"200": "OK"}, {"500": "INTERNAL SERVER ERROR"}, {"400": "BAD REQUEST"}],
+    responses=[
+        {"409": "CONFLICT"},
+        {"200": "OK"},
+        {"500": "INTERNAL SERVER ERROR"},
+        {"400": "BAD REQUEST"},
+    ],
     examples=[
-        OpenApiExample(
-            name="Request Example",
-            value={
-                "phone_number": "+79889889898"
-            }
-        )
-    ]
+        OpenApiExample(name="Request Example", value={"phone_number": "+79889889898"})
+    ],
 )
 class SendSMSView(GenericAPIView):
 
@@ -52,40 +54,69 @@ class SendSMSView(GenericAPIView):
         phone_number = serializer_instance.data.get("phone_number")
 
         if not phone_number:
-            return Response({'error': 'Missing phone number'}, status=status.HTTP_400_BAD_REQUEST)
-        
+            return Response(
+                {"error": "Missing phone number"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
         cache_prefix = getattr(settings, "SMS_CACHE_PREFIX", "SMS_CACHE")
-        cache_key = f'{cache_prefix}_{phone_number}'
+        cache_key = f"{cache_prefix}_{phone_number}"
         cached_data = cache.get(cache_key)
 
         if cached_data:
-            remaining_time = cached_data.get('expiration_time') - datetime.now()
+            remaining_time = cached_data.get("expiration_time") - datetime.now()
             if remaining_time.total_seconds() > 0:
-                return Response({'message': f'Please wait. Time remaining: {int(remaining_time.total_seconds())} seconds'}, status=status.HTTP_409_CONFLICT)
+                return Response(
+                    {
+                        "message": f"Please wait. Time remaining: {int(remaining_time.total_seconds())} seconds"
+                    },
+                    status=status.HTTP_409_CONFLICT,
+                )
 
-        message = "".join(rd.choices(digits, k=4))
+        code = "".join(rd.choices(digits, k=4))
+        message = f"Ваш код: {code}. Никому не сообщайте его!"
+        print(message)
 
-        try:
-            response = requests.get('https://sms.ru/sms/send', params={
-                'api_id': api_key,
-                'to': phone_number,
-                'msg': message,
-                'json': 1  # to receive response in JSON format
-            })
-            response_data = response.json()
+        # try:
+        #     response = requests.get('https://sms.ru/sms/send', params={
+        #         'api_id': api_key,
+        #         'to': phone_number,
+        #         'msg': message,
+        #         'json': 1  # to receive response in JSON format
+        #     })
+        #     response_data = response.json()
 
-            if response_data['status'] == 'OK':
-                cache.set(cache_key, {'expiration_time': datetime.now() + timedelta(seconds=60), 'message': message}, timeout=None)  # Cache for 60 seconds
-                return Response({'success': True}, status=status.HTTP_200_OK)
-            else:
-                return Response({'error': response_data['status_text']}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+        #     if response_data['status'] == 'OK':
+        cache.set(
+            cache_key,
+            {
+                "expiration_time": datetime.now() + timedelta(seconds=code_lifetime),
+                "code": code,
+            },
+            timeout=code_lifetime,
+        )  # Cache for 60 seconds
+        #         return Response({'success': True}, status=status.HTTP_200_OK)
+        #     else:
+        #         return Response({'error': response_data['status_text']}, status=status.HTTP_400_BAD_REQUEST)
+        # except Exception as e:
+        return Response(status=status.HTTP_200_OK)
 
 
 @extend_schema(
-    tags=['Account']
+    tags=["Account"],
+    description="Проверка кода подтверждения.",
+    summary="Проверка кода подтверждения",
+    responses=[
+        {"409": "CONFLICT"},
+        {"200": "OK"},
+        {"500": "INTERNAL SERVER ERROR"},
+        {"400": "BAD REQUEST"},
+    ],
+    examples=[
+        OpenApiExample(
+            name="Request Example",
+            value={"phone_number": "+79889889898", "code": "4378"},
+        )
+    ],
 )
 class VerifyConfirmCode(GenericAPIView):
     permission_classes = [AllowAny]
@@ -93,23 +124,41 @@ class VerifyConfirmCode(GenericAPIView):
 
     def post(self, request):
 
-        phone_number = request.data.get('phone_number')
-        phone_serializer = PhoneSerializer(data={'phone_number': phone_number})
+        phone_number = request.data.get("phone_number")
+        phone_serializer = PhoneSerializer(data={"phone_number": phone_number})
+        phone_serializer.is_valid(raise_exception=True)
 
-        if not phone_serializer.is_valid():
-            return Response({"message": "Invalid phone number"}, status=status.HTTP_400_BAD_REQUEST)
+        code = request.data.get("code")
+        serializer = self.serializer_class(data={"code": code})
+        serializer.is_valid(raise_exception=True)
 
-        code = request.data.get('code')
-        
-        serializer = self.serializer_class(data={'code': code})
-        phone_number = cache.get(code)
-        
-        if not serializer.is_valid() or not phone_number:
-            return Response({'message': 'Incorrect confirmation code'}, status=status.HTTP_400_BAD_REQUEST)
+        cached_key = f"{settings.SMS_CACHE_PREFIX}_{phone_number}"
+        cached_data = cache.get(cached_key, {})
+        cached_code = cached_data.get("code")
 
-        user, _ = CustomUser.objects.get_or_create(phone_number=phone_number)
+        if not cached_data or code != cached_code:
+            return Response(
+                {"message": "Invalid confirmation code"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-        user.is_active = True
-        user.save()
+        user, created = CustomUser.objects.get_or_create(
+            phone=phone_number, defaults={"username": phone_number}
+        )
+
+        if not user.is_active:
+            user.is_active = True
+            if created:
+                user.set_password(phone_number)
+
+            user.save()
+
         serialized_tokens = TokenObtainPairSerializer.get_token(user)
-        return Response({'message': "User successfully activated", **serialized_tokens}, status=status.HTTP_200_OK)
+        return Response(
+            {
+                "message": "User successfully activated",
+                "access": str(serialized_tokens.access_token),
+                "refresh": str(serialized_tokens),
+            },
+            status=status.HTTP_200_OK,
+        )
