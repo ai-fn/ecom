a22ad2c1c6cf9a79e8d46625724f56ec9afca5ca
Андрей Жиров <a.zhirov@alta-west.ru>
2024-03-27 10:12:27 +0300

    14003: add expided_at field to Token pair

--- api/mixins.py
+++ api/mixins.py
@@ -38,5 +38,6 @@ class TokenExpiredTimeMixin:
     def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
         data = super().validate(attrs)
 
-        data['expired_at'] = time.time() + settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'].total_seconds()
+        data['access_expired_at'] = time.time() + settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'].total_seconds()
+        data['refresh_expired_at'] = time.time() + settings.SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'].total_seconds()
         return data
