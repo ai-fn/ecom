f728748b45c4369bd9f14c4832f18c27146b89b2
Андрей Жиров <a.zhirov@alta-west.ru>
2024-04-27 11:45:31 +0300

    14545: update serializer

--- api/serializers/sidebar_menu_item.py
+++ api/serializers/sidebar_menu_item.py
@@ -1,10 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from shop.models import SideBarMenuItem
 
 
 class SideBarMenuItemSerializer(ModelSerializer):
+
+    icon = SerializerMethodField()
     
     class Meta:
         model = SideBarMenuItem
         exclude = ["created_at", "updated_at"]
+    
+    def get_icon(self, obj):
+        return obj.icon.url if obj.icon else None
