63a13e4384d9805ee6a7b526315e2f2ce06ae6ae
Андрей Жиров <a.zhirov@alta-west.ru>
2024-05-28 12:31:39 +0300

    test commit

--- api/filters.py
+++ api/filters.py
@@ -25,38 +25,38 @@ class ProductFilter(GeneralSearchMixin, filters.FilterSet):
     )
     city_domain = filters.CharFilter(method='filter_city_domain')
 
-    @cached_property
-    def qs(self):
-        session = self.request.session
+    # @cached_property
+    # def qs(self):
+    #     session = self.request.session
 
-        if not any(param in self.request.query_params for param in self.Meta.fields):
-            if "filtered_products" in session:
-                print("deleting filtered products from session...")
-                del self.request.session["filtered_products"]
-            return Product.objects.all()
-        else:
-            if 'filtered_products' in session:
-                print("return filtered products from session...")
-                initial_queryset = Product.objects.filter(pk__in=session['filtered_products'])
-            else:
-                print("return all products...")
-                initial_queryset = Product.objects.all()
+    #     if not any(param in self.request.query_params for param in self.Meta.fields):
+    #         if "filtered_products" in session:
+    #             print("deleting filtered products from session...")
+    #             del self.request.session["filtered_products"]
+    #         return Product.objects.all()
+    #     else:
+    #         if 'filtered_products' in session:
+    #             print("return filtered products from session...")
+    #             initial_queryset = Product.objects.filter(pk__in=session['filtered_products'])
+    #         else:
+    #             print("return all products...")
+    #             initial_queryset = Product.objects.all()
             
-            return self.filter_queryset(initial_queryset)
+    #         return self.filter_queryset(initial_queryset)
 
 
-    def get_queryset(self):
-        session = self.request.session
-        if 'filtered_products' in session:
-            initial_queryset = Product.objects.filter(pk__in=session['filtered_products'])
-        else:
-            initial_queryset = Product.objects.all()
+    # def get_queryset(self):
+    #     session = self.request.session
+    #     if 'filtered_products' in session:
+    #         initial_queryset = Product.objects.filter(pk__in=session['filtered_products'])
+    #     else:
+    #         initial_queryset = Product.objects.all()
 
-        # Сохранение начального списка продуктов в сессии
-        session['filtered_products'] = list(initial_queryset.values_list('pk', flat=True))
-        session.modified = True
+    #     # Сохранение начального списка продуктов в сессии
+    #     session['filtered_products'] = list(initial_queryset.values_list('pk', flat=True))
+    #     session.modified = True
 
-        return initial_queryset
+    #     return initial_queryset
 
 
     class Meta:
@@ -64,27 +64,27 @@ class ProductFilter(GeneralSearchMixin, filters.FilterSet):
         fields = ['search', 'category', 'brand_slug', 'price_lte', 'price_gte', 'characteristics', 'city_domain']
 
 
+    # def filter_search(self, queryset, name, value):
+    #     return self.apply_filter(queryset, self._filter_search, name, value)
+
+    # def filter_category(self, queryset, name, value):
+    #     return self.apply_filter(queryset, self._filter_category, name, value)
+
+    # def filter_characteristics(self, queryset, name, value):
+    #     return self.apply_filter(queryset, self._filter_characteristics, name, value)
+
+    # def filter_city_domain(self, queryset, name, value):
+    #     return self.apply_filter(queryset, self._filter_city_domain, name, value)
+
+    # def apply_filter(self, queryset, filter_func, name, value):
+    #     """Общий метод для применения фильтра и обновления сессии."""
+    #     queryset = self.get_queryset()
+    #     queryset = filter_func(queryset, name, value)
+    #     self.request.session['filtered_products'] = list(queryset.values_list('pk', flat=True))
+    #     self.request.session.modified = True
+    #     return queryset
+
     def filter_search(self, queryset, name, value):
-        return self.apply_filter(queryset, self._filter_search, name, value)
-
-    def filter_category(self, queryset, name, value):
-        return self.apply_filter(queryset, self._filter_category, name, value)
-
-    def filter_characteristics(self, queryset, name, value):
-        return self.apply_filter(queryset, self._filter_characteristics, name, value)
-
-    def filter_city_domain(self, queryset, name, value):
-        return self.apply_filter(queryset, self._filter_city_domain, name, value)
-
-    def apply_filter(self, queryset, filter_func, name, value):
-        """Общий метод для применения фильтра и обновления сессии."""
-        queryset = self.get_queryset()
-        queryset = filter_func(queryset, name, value)
-        self.request.session['filtered_products'] = list(queryset.values_list('pk', flat=True))
-        self.request.session.modified = True
-        return queryset
-
-    def _filter_search(self, queryset, name, value):
         domain = self.request.query_params.get("city_domain")
         search_results = self.g_search(
             value, domain, exclude_=("review", "brand", "category")
@@ -97,7 +97,7 @@ class ProductFilter(GeneralSearchMixin, filters.FilterSet):
             queryset = Product.objects.none()
         return queryset
 
-    def _filter_category(self, queryset, name, value):
+    def filter_category(self, queryset, name, value):
         categories = [value]
         try:
             category_instance = Category.objects.get(slug=value)
@@ -109,7 +109,7 @@ class ProductFilter(GeneralSearchMixin, filters.FilterSet):
         queryset = queryset.filter(Q(category__slug__in=categories) | Q(additional_categories__slug=value))
         return queryset 
 
-    def _filter_characteristics(self, queryset, name, value):
+    def filter_characteristics(self, queryset, name, value):
         char_values = {}
         filter_conditions = Q()
         if value:
@@ -122,12 +122,12 @@ class ProductFilter(GeneralSearchMixin, filters.FilterSet):
                     char_values[char_name].append(char_value)
 
         for char in char_values:
-            filter_conditions |= Q(characteristic_values__characteristic__slug=char, characteristic_values__value__in=char_values[char])
+            filter_conditions &= Q(characteristic_values__characteristic__slug=char, characteristic_values__value__in=char_values[char])
         
         queryset = queryset.filter(filter_conditions, characteristic_values__characteristic__for_filtering=True).distinct()
         return queryset
 
-    def _filter_city_domain(self, queryset, name, value):
+    def filter_city_domain(self, queryset, name, value):
         price_gte = self.request.query_params.get("price_gte")
         price_lte = self.request.query_params.get("price_lte")
         if value and (price_gte or price_lte):
