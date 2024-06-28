2c5815d413ffa8b43888bfc8b97204f75b37e274
Андрей Жиров <a.zhirov@alta-west.ru>
2024-06-28 13:01:23 +0300

    update: update serializer and swagger for main-page-slider

--- api/serializers/main_page_slider_image.py
+++ api/serializers/main_page_slider_image.py
@@ -5,10 +5,14 @@ from shop.models import MainPageSliderImage
 class MainPageSliderImageSerializer(serializers.ModelSerializer):
 
     image = serializers.SerializerMethodField()
+    tiny_image = serializers.SerializerMethodField()
 
     class Meta:
         model = MainPageSliderImage
-        fields = ['id', 'order', 'link', 'title', "description",  'button_text', "image", "thumb_img",]
+        fields = ['id', 'order', 'link', 'title', "description",  'button_text', "image", "thumb_img", "tiny_image",]
     
     def get_image(self, obj) -> str | None:
         return obj.image.url if obj.image else None
+    
+    def get_tiny_image(self, obj) -> str | None:
+        return obj.tiny_image.url if obj.tiny_image else None
