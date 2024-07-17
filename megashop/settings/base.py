1b76ba7486997b0b9768b4d240b0aaed588cf730
Андрей Жиров <a.zhirov@alta-west.ru>
2024-07-17 09:29:38 +0300

    update: update login url

--- megashop/settings/base.py
+++ megashop/settings/base.py
@@ -259,7 +259,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-LOGIN_URL = "login"
+LOGIN_URL = "/admin/login/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
