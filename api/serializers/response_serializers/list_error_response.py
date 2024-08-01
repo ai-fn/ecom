3192ed02772d5f45b9af2d8b9d91b05abfa13db0
Андрей Жиров <a.zhirov@alta-west.ru>
2024-08-01 11:59:20 +0300

    fix: fix typo

--- api/serializers/response_serializers/list_error_response.py
+++ api/serializers/response_serializers/list_error_response.py
@@ -2,4 +2,4 @@ from rest_framework.serializers import Serializer, DictField, CharField
 
 
 class ListErrorResponseSerializer(Serializer):
-    error = DictField(child=CharField)
+    error = DictField(child=CharField())
