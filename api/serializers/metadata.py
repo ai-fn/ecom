7e0d5a842948a906987ca7f7284827d3386f933f
andrej <a.zhirov@alta-west.ru>
2024-09-11 18:27:18 +0300

    update: update metadata image

--- api/serializers/metadata.py
+++ api/serializers/metadata.py
@@ -7,19 +7,22 @@ from django.core.cache import cache
 from loguru import logger
 
 
+from account.models import City
 from api.serializers import ActiveModelSerializer
 
-from rest_framework.serializers import Serializer, CharField
+from rest_framework.serializers import Serializer, CharField, SerializerMethodField
 
 from shop.models import OpenGraphMeta, Setting, SettingChoices
 from shop.services.metadata_service import MetaDataService
 
 META_IMAGE_PATH_CACHE_KEY = "META_IMAGE_PATH"
-META_IMAGE_SIZE_CACHE_KEY = "META_IMAGE_SIZE"
 META_IMAGE_SIZE_REMINING_TIME = getattr(
     settings, "META_IMAGE_SIZE_REMINING_TIME", 3600 * 24
 )  # время кеширования на сутки
 
+
+def get_meta_image_size_cache_key(img_name: str) -> str:
+    return f"META_IMAGE_SIZE_{img_name}"
 
 class ImageMetaDataSerializer(Serializer):
     image = CharField()
@@ -28,14 +31,16 @@ class ImageMetaDataSerializer(Serializer):
     def to_representation(self, instance):
         data = super().to_representation(instance)
         data["image"] = path if (path := data["image"]) and path.startswith("/") else "/" + path
-        image_size = cache.get(META_IMAGE_SIZE_CACHE_KEY)
-        if image_size:
+        cache_key = get_meta_image_size_cache_key(data["image"])
+        
+        image_size = cache.get(cache_key)
+        if not image_size:
 
             try:
                 image_path = settings.BASE_DIR / data["image"][1:]
                 with Image.open(image_path) as image:
                     image_size = {"width": image.width, "height": image.height}
-                    cache.set(META_IMAGE_SIZE_CACHE_KEY, image_size, META_IMAGE_SIZE_REMINING_TIME)
+                    cache.set(cache_key, image_size, META_IMAGE_SIZE_REMINING_TIME)
 
             except Exception as e:
                 image_size = {"width": None, "height": None}
@@ -48,6 +53,8 @@ class ImageMetaDataSerializer(Serializer):
 
 class OpenGraphMetaSerializer(ActiveModelSerializer):
 
+    images = SerializerMethodField()
+
     class Meta:
         model = OpenGraphMeta
         fields = [
@@ -55,18 +62,40 @@ class OpenGraphMetaSerializer(ActiveModelSerializer):
             "description",
             "keywords",
             "url",
+            "images",
             "locale",
             "site_name",
         ]
 
+    def get_images(self, obj) -> list:
+        image = None
+        result = []
+        if (model := obj.content_type.model) and model in ("product", "category", "brand"):
+            if model == "product":
+                img_field_name = "catalog_image"
+            elif model == "brand":
+                img_field_name = "icon"
+            else:
+                img_field_name = "image"
+            
+            img_attr = getattr(obj.content_object, img_field_name)
+
+            if img_attr.name:
+                image = {"image": f"media/{img_attr.name}"}
+        else:
+            image = self.get_meta_image()
+
+        if image:
+            result.append(ImageMetaDataSerializer(image).data)
+
+        return result
+
 
     def get_meta_image(self) -> dict[str, str] | None:
         result = None
+
         cached_data = cache.get(META_IMAGE_PATH_CACHE_KEY)
         if not cached_data:
-            result = cached_data
-
-        else:
             meta_image_setting = Setting.objects.filter(predefined_key=SettingChoices.OPEN_GRAPH_META_IMAGE).first()
             if not meta_image_setting:
                 logger.info("Setting for openGraphMeta Image not found.")
@@ -75,6 +104,8 @@ class OpenGraphMetaSerializer(ActiveModelSerializer):
                 cache.set(
                     META_IMAGE_PATH_CACHE_KEY, result, META_IMAGE_SIZE_REMINING_TIME
                 )
+        else:
+            result = cached_data
 
         return result
 
@@ -94,21 +125,20 @@ class OpenGraphMetaSerializer(ActiveModelSerializer):
         for field in fields:
             data[field] = result.get(field)
 
+        if not city_domain:
+            city_domain = City.get_default_city().domain
+
+        url = f"https://{city_domain}"
         if instance.content_type.model in ("product", "category"):
-            url = f"/{instance.content_object.get_absolute_url()}/"
+            url += f"/{instance.content_object.get_absolute_url()}/"
         else:
-            url = data.get("url")
+            url += data.get("url")
 
         keywords = data.get("keywords").split(",")
         site_name = data.get("site_name") or os.environ.get("SHOP_NAME", "site name")
 
         if keywords:
             keywords = [x.strip() for x in keywords]
-
-        images = []
-        meta_image_data = self.get_meta_image()
-        if meta_image_data:
-            images.append(ImageMetaDataSerializer(meta_image_data).data)
 
         return {
             "title": data.get("title"),
@@ -117,7 +147,7 @@ class OpenGraphMetaSerializer(ActiveModelSerializer):
             "openGraph": {
                 "url": url,
                 "siteName": site_name,
-                "images": images,
+                "images": data.get("images"),
                 "locale": data.get("locale"),
                 "type": "website",
             },
