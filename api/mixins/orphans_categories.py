cdec4b120eb2f5f25c103ef0c254653d3ebf4889
Андрей Жиров <a.zhirov@alta-west.ru>
2024-10-14 17:19:34 +0300

    updat: updated orphans categories

--- api/mixins/orphans_categories.py
+++ api/mixins/orphans_categories.py
@@ -3,11 +3,13 @@ from django.db.models import QuerySet
 
 class GetOrphanCategories:
     def get_orphan_categories(self, queryset: QuerySet, domain: str):
-        queryset = queryset.filter(
-            is_active=True,
-            is_visible=True,
-            parent__isnull=True,
-            products__isnull=False,
-            children__products__prices__city_group__cities__domain=domain,
-        )
-        return queryset
+        parents = []
+        top_level = queryset.filter(level=0)
+        for parent in top_level:
+            if parent.get_descendants().filter(
+                is_active=True, is_visible=True,
+                products__isnull=False,
+                products__prices__city_group__cities__domain=domain
+            ).exists():
+                parents.append(parent.id)
+        return queryset.filter(pk__in=parents)
