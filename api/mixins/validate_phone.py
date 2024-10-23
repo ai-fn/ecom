073dc9c301bc90816d4ccaed2b5b0e143e2ba96b
Андрей Жиров <a.zhirov@alta-west.ru>
2024-10-23 14:14:26 +0300

    update: translate errors, fix typos in email templates

--- api/mixins/validate_phone.py
+++ api/mixins/validate_phone.py
@@ -2,22 +2,20 @@ import phonenumbers
 from rest_framework.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-from account.models import CustomUser
-
 
 class ValidatePhoneNumberMixin:
 
     def validate_phone(self, value):
         if instance := getattr(self, "instance", None):
             if getattr(instance, "phone", None) == value:
-                raise ValidationError(_("Phone already confirmed"))
+                raise ValidationError(_("Телефон уже подтверждён"))
 
         phone = value
         try:
             parsed_number = phonenumbers.parse(phone, None)
             if not phonenumbers.is_valid_number(parsed_number):
-                raise ValidationError(_("Invalid phone number."))
+                raise ValidationError(_("Неверный номер телефона."))
         except phonenumbers.phonenumberutil.NumberParseException:
-            raise ValidationError(_("Invalid phone number."))
+            raise ValidationError(_("Неверный номер телефона."))
 
         return value
