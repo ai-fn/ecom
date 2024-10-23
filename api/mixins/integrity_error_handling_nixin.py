db1b6c9f35857d0e7249662451d5d4e87167aafc
Андрей Жиров <a.zhirov@alta-west.ru>
2024-10-23 13:17:09 +0300

    fix: fix error handling

--- api/mixins/integrity_error_handling_nixin.py
+++ api/mixins/integrity_error_handling_nixin.py
@@ -1,3 +1,22 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+APIView.dispatch
+
+
 class IntegrityErrorHandlingMixin:
     def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
+        try:
+            return super().dispatch(request, *args, **kwargs)
+        except Exception as err:
+            response = Response({"error": str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            if isinstance(response, Response):
+                if not getattr(request, 'accepted_renderer', None):
+                    neg = super().perform_content_negotiation(request, force=True)
+                    request.accepted_renderer, request.accepted_media_type = neg
+
+            response.accepted_renderer = request.accepted_renderer
+            response.accepted_media_type = request.accepted_media_type
+            response.renderer_context = super().get_renderer_context()
+            return response
