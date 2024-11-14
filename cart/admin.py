aebb88caae1664c503dcc92a5baeee48ab8d6f7e
Андрей Жиров <a.zhirov@alta-west.ru>
2024-11-14 11:07:31 +0300

    hotfix: added Pickup model to admin site

--- cart/admin.py
+++ cart/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CartItem, Order, ProductsInOrder
+from .models import CartItem, Order, ProductsInOrder, PickupPoint
 from api.mixins import ActiveAdminMixin
 
 
@@ -27,6 +27,20 @@ class OrderAdmin(ActiveAdminMixin, admin.ModelAdmin):
         return ProductsInOrder.objects.filter(order=obj).count()
 
     quantity.short_description = "Количество позиций"
+
+
+@admin.register(PickupPoint)
+class PickupPointAdmin(ActiveAdminMixin, admin.ModelAdmin):
+    ordering = ("created_at",)
+    list_display = (
+        "id",
+        "address",
+        "coord_x",
+        "coord_y",
+        "created_at",
+        "is_active",
+    )
+    list_filter = ("is_active",)
 
 
 @admin.register(CartItem)
